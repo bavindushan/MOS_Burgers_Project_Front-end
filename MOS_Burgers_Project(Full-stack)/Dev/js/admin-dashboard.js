7c08da00d19e33e2917b8f3cdf0d01bcf547abf6
bavindushan <bavindushan65@gmail.com>
2024-12-17 13:34:15 +0530

    create report management page

--- MOS_Burgers_Project(Full-stack)/Dev/js/admin-dashboard.js
+++ MOS_Burgers_Project(Full-stack)/Dev/js/admin-dashboard.js
@@ -9,7 +9,7 @@ new Chart(ctx, {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December'],
         datasets: [{
             label: 'Annual Sales Summary',
-            data: [12, 19, 3, 5, 2, 3,12,23,45,56,66,78],
+            data: [12, 19, 25, 5, 27, 39,22,23,45,56,66,78],
             borderWidth: 1
         }]
     },
